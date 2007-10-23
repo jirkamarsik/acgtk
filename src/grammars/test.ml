34c89766761332b09b9d55107d32c6e10b342c7e
pogodall <pogodall@8fde8617-4931-0410-9586-bc38d6d3f018>
2007-10-23 14:41:20 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/acg/dev/trunk@18 8fde8617-4931-0410-9586-bc38d6d3f018

--- src/grammars/test.ml
+++ src/grammars/test.ml
@@ -7,8 +7,8 @@ let parse filename =
   let in_ch = open_in filename in
   let lexbuf = Lexing.from_channel in_ch in
     try
-      let sg = fst (List.hd (Parser.signature Lexer.lexer lexbuf)) in
-	Printf.printf "%s\n" (Abstract_syntax.Abstract_sig.to_string sg)
+      let sgs = fst (List.hd (Parser.signatures Lexer.lexer lexbuf)) in
+	List.iter (fun sg -> Printf.printf "%s\n" (Abstract_syntax.Abstract_sig.to_string sg)) sgs
     with
       | Error.Error e -> Printf.fprintf stderr "Error: %s\n" (Error.error_msg e lexbuf "test.dat")
       | Dyp.Syntax_error -> Printf.fprintf stderr "Dyp: %s\n" (Error.error lexbuf "test.dat")
