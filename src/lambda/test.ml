3a5e19a45846514c870b45e4b604821bb8d52bc4
maarek <maarek@8fde8617-4931-0410-9586-bc38d6d3f018>
2008-05-21 08:48:32 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/acg/dev/trunk@75 8fde8617-4931-0410-9586-bc38d6d3f018

--- src/lambda/test.ml
+++ src/lambda/test.ml
@@ -1,37 +1,45 @@
 let options = []
 
+let env = ref Abstract_syntax.Environment.empty
+
 let usg_msg = ""
 
 
 let parse filename =
-  let sgs = Data_parsing.Data_parsing.signature filename in
-    Abstract_syntax.Environment.Env.iter 
-      (fun _ sg ->
-	 try
+  env := Data_parsing.Data_parsing.data filename !env
+
+
+let term_parsing env =
+(*  let sgs = Data_parsing.Data_parsing.signature filename in*)
+    Abstract_syntax.Environment.iter 
+      (fun content ->
 	   let () = Printf.printf "\n\nResultat typecheck : \n" in
-	   let Abstract_syntax.Abstract_sig.Signature(name,y) = sg in
-	   let t = (Typechecker.typecheck (name,y)) in
-	   let Signature.Abstract_typ.Signature (name,size,_,trie,content) = t in
+	   let Abstract_syntax.Environment.Signature(sg) = 
+	     content in
+	 try
+	   let Abstract_syntax.Abstract_sig.Signature((name,_),y) = sg in
+	   let (t,l) = (Typechecker.typecheck (name,y)) in
+	   let Sign.Sign.Signature (name,size,_,trie,content) = t in
 	   let list_decl = Tries.Tries.content trie in
-	     Printf.printf "Signature \"%s\": [\n" name;
+	     Printf.printf "Sign \"%s\": [\n" name;
 	     List.iter
 	       (fun x -> (match x with
-			      Signature.Abstract_typ.Term_def(s,i,kd,wf,typ) -> 
-				print_string (s^" = ");
-				Typechecker.display_wfterm t wf;
+			      Sign.Sign.Term_def(s,i,kd,wf,typ) -> 
+				print_string (s^" =PP ");
+				print_string (Sign.Sign.pretty_print wf);
 				print_string " : ";
 				Typechecker.display_typ_tdef t typ;
 				print_string " ;\n ";
-			    | Signature.Abstract_typ.Term_decl(s,i,kd,typ) -> 
+			    | Sign.Sign.Term_decl(s,i,kd,typ) -> 
 				print_string (s^" : ");
 				Typechecker.display_typ_tdef t typ;
 				print_string " ;\n ";
-			    | Signature.Abstract_typ.Type_def(s,i,typ) -> 
+			    | Sign.Sign.Type_def(s,i,typ) -> 
 				print_string (s^" : ");
 				Typechecker.display_typ_tdef t typ;
 				print_string " ;\n ";
-			    | Signature.Abstract_typ.Type_decl
-				(s,i,Signature.Abstract_typ.K tdl) -> 
+			    | Sign.Sign.Type_decl
+				(s,i,Lambda.Lambda.K tdl) -> 
 				print_string (s^" : K[");
 				  List.iter (Typechecker.display_typ_tdef t) tdl;
 				  print_string "] ;\n ";))
@@ -39,9 +47,14 @@ let parse filename =
 	     print_string"]\n"
 	 with
 	   | Error.Error e -> 
-	       let () = Printf.fprintf stderr "Error: in signature\"%s\"\n" (Error.error_msg e (Abstract_syntax.Abstract_sig.name sg)) in
+	       let name,_ = Abstract_syntax.Abstract_sig.name sg in
+	       let () = Printf.fprintf stderr "Error: in signature\"%s\"\n" (Error.error_msg e name) in
 		 ())
-      sgs
+(*      sgs*) env
 	  
 	  
-let () = Arg.parse options parse usg_msg
+(* let () = Arg.parse options parse usg_msg *)
+	
+let () =
+  let () = Arg.parse options parse usg_msg in
+    term_parsing !env
