77b182981fcd9b3fa94e8d6b4437d70930fd58e6
pogodall <pogodall@8fde8617-4931-0410-9586-bc38d6d3f018>
2014-09-05 11:04:52 +0000

    Added formatting of the welcome message for acg/acg.opt
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/acg/dev/trunk@557 8fde8617-4931-0410-9586-bc38d6d3f018

--- src/scripting/acg.ml
+++ src/scripting/acg.ml
@@ -46,11 +46,10 @@ let options =
 let usg_msg = Printf.sprintf "%s [options] file1 file2 ...\n\nThis will parse the files which are supposed to be files acripting commands and then run the ACG command interpreter." Sys.executable_name
 
 
-
-let welcome_msg = 
-  Printf.sprintf
-    "\n\t\t\tWelcome to the ACG toplevel\n\t\t\t    Version %s\n\t\t\t\t©INRIA 2008-2014\nPlease send your comments or bug reports or featrure requests to sylvain.pogodalla@inria.fr\n\n\nType\n\t\thelp ;\nto get help.\n\n\n\n"
-    Version.version
+let welcome_string = "Welcome to the ACG toplevel"
+let version_string = Printf.sprintf "Version %s" Version.version
+let copyright_string = "©INRIA 2008-2014"
+let bug_string = "Please send your comments or bug reports or feature requests to sylvain.pogodalla@inria.fr"
 
 
 let env = ref E.empty
@@ -62,19 +61,52 @@ let resize_terminal () =
   else
     ()
 
+let welcome_msg () =
+  let () = resize_terminal () in
+  let l = 
+    let l = Format.get_margin () in
+    if l > 1000 then
+      ((String.length bug_string)+10)
+    else
+      l in
+  let _ = Format.flush_str_formatter () in
+  let () = Format.printf "@[<v>" in
+  let () = 
+    List.iter
+      (fun s -> 
+	let prepend =
+	  try
+	    (String.make ((l-(String.length s))/2) ' ')
+	  with
+	  | Invalid_argument _ -> "" in
+	Format.printf "@[<h>%s%s@]@," prepend s)
+      [welcome_string;version_string;copyright_string;bug_string] in
+  Format.printf "@,@[<v>@[<v15>Type@,help;@]@,@[to@ get@ help.@]@]@,@]@.@?"
+    
+    
+let printed_welcome_message = ref false
+
+let print_welcome_message () = 
+  if !printed_welcome_message then
+    ()
+  else
+    let () = welcome_msg ()in
+    printed_welcome_message := true
+
 
 let anon_fun s =
+  let () = print_welcome_message () in
   let () = resize_terminal () in
   env := P.parse_file s !dirs !env
   
 let _ =
-  let () = Printf.printf "%s%!" welcome_msg in
   (* ANSITerminal get the size info from stdin In case of redirection,
      the latter may not be set. That's why it is first duplicated and
      stdin is then duplicated from stdout *)
   let stdin_tmp=Unix.dup Unix.stdin in
   let stdin_tmp_in_ch = Unix.in_channel_of_descr stdin_tmp in
   let () = Unix.dup2 Unix.stdout Unix.stdin in
+  let () = print_welcome_message () in
   let () = Arg.parse options anon_fun usg_msg in
   let continue = ref true in
   let () = resize_terminal () in
